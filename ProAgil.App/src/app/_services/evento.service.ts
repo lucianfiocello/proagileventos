2cfe0ab6fa66359d5955daf411a2e936726c9683
Lucian Rezende <lucianfiocello@gmail.com>
2021-11-07 13:37:29 -0300

    Criado o evento service

---
+++ ProAgil.App/src/app/_services/evento.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Evento } from '../_models/evento';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventoService {
+  baseUrl = 'http://localhost:5000/api/eventos';
+  constructor(private http: HttpClient) { }
+
+  getTodosEventos(): Observable<Evento[]>{
+    return this.http.get<Evento[]>(this.baseUrl);
+  }
+
+  getEventosPorTema(tema: string): Observable<Evento[]>{
+    return this.http.get<Evento[]>(`${this.baseUrl}/obterPorTema/${tema}`);
+  }
+
+  getEventoPorId(id: number): Observable<Evento>{
+    return this.http.get<Evento>(`${this.baseUrl}/${id}`);
+  }
+}
